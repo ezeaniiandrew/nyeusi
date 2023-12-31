90d355ef15bf75fb0803915ed6f68a79782e1793
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-12-31 08:25:27 +0100

    site update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/routes.jsx";
+
 function App() {
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+
+const folders = fs.readdirSync("./src", { withFileTypes: true });
+const fileNames = folders
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
+const filePaths = fileNames.reduce((acc, currentPath) => {
+  const key = `@/${[currentPath]}`;
+
+  return {
+    ...acc,
+    [key]: `/${currentPath === "src" ? currentPath : "src/" + currentPath}`,
+  };
+}, {});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      ...filePaths,
+    },
+  },
+});
