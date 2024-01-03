57bdbd1a1165203841f3e27626e7d904ba57708f
Andrew Ezeani <ezeaniiandrew@gmail.com>
2024-01-03 18:21:46 +0100

    updated contact pages

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,40 @@
+import styles from "./contact.module.scss";
+
 function Contact() {
   return (
-    <div>
-      <p>Contact Us</p>
-    </div>
+    <>
+      <article className={styles.col_1}>
+        <div>
+          <h2>Contact Us</h2>
+          <p>
+            At Nyeusi Interior Design House, we're here to help you realize your
+            vision of dark luxurious interiors. Whether you have questions, need
+            to discuss your project, or simply want to connect with us, we're
+            just a message or call away.
+          </p>
+        </div>
+        <div>
+          <ul>
+            <li>+254712345678</li>
+            <li>nyeusiinteriordesign@gmail.com</li>
+            <li>tom mboya street, nairobi, kenya.</li>
+          </ul>
+          <div>
+            <span>icon</span>
+            <span>icon</span>
+            <span>icon</span>
+          </div>
+        </div>
+      </article>
+      <div className="col2">form</div>
+      <div>image</div>
+      <p>
+        Feel free to connect with Nyeusi Interior Design House for inquiries,
+        collaborations, or to share your vision for your dark luxurious
+        interiors. We eagerly anticipate the opportunity to bring your dreams to
+        life.
+      </p>
+    </>
   );
 }
 
