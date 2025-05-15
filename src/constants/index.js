d3792ab1106759314a02292980ec1678b652bf1c
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-05-15 22:37:38 +0100

    feat: complete About page with new content and images

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,6 +8,27 @@ import mobilePastProject2 from "@/assets/mobile-home-project-image-2.jpg";
 import mobilePastProject3 from "@/assets/mobile-home-project-image-3.jpg";
 import mobilePastProject4 from "@/assets/mobile-home-project-image-4.jpg";
 
+import ourServicesCardOneImageBig from "@/assets/our-service-card-one-big.webp";
+import ourServicesCardOneImageSmall from "@/assets/our-service-card-one-small.webp";
+import ourServicesCardTwoImageBig from "@/assets/our-service-card-two-big.webp";
+import ourServicesCardTwoImageSmall from "@/assets/our-service-card-two-small.webp";
+import ourServicesCardThreeImageBig from "@/assets/our-service-card-three-big.webp";
+import ourServicesCardThreeImageSmall from "@/assets/our-service-card-three-small.webp";
+import ourServicesCardFourImageBig from "@/assets/our-service-card-four-big.webp";
+import ourServicesCardFourImageSmall from "@/assets/our-service-card-four-small.webp";
+import ourServicesCardFiveImageBig from "@/assets/our-service-card-five-big.webp";
+import ourServicesCardFiveImageSmall from "@/assets/our-service-card-five-small.webp";
+import ourServicesCardSixImageBig from "@/assets/our-service-card-six-big.webp";
+import ourServicesCardSixImageSmall from "@/assets/our-service-card-six-small.webp";
+import ourProcessCardOneImageBig from "@/assets/our-process-card-one-big.webp";
+import ourProcessCardOneImageSmall from "@/assets/our-process-card-one-small.webp";
+import ourProcessCardTwoImageBig from "@/assets/our-process-card-two-big.webp";
+import ourProcessCardTwoImageSmall from "@/assets/our-process-card-two-small.webp";
+import ourProcessCardThreeImageBig from "@/assets/our-process-card-three-big.webp";
+import ourProcessCardThreeImageSmall from "@/assets/our-process-card-three-small.webp";
+import ourProcessCardFourImageBig from "@/assets/our-process-card-four-big.webp";
+import ourProcessCardFourImageSmall from "@/assets/our-process-card-four-small.webp";
+
 export const navLinks = [
   { path: "/", name: "home" },
   { path: "/projects", name: "projects" },
@@ -37,3 +58,100 @@ export const pastProjectsData = [
     images: [mobilePastProject4, desktopPastProject4],
   },
 ];
+
+export const aboutUsCardsData = {
+  services: [
+    {
+      title: "1: LUXURIOUS INTERIOR DESIGN",
+      description:
+        "Our core service revolves around creating dark luxurious interiors that redefine the concept of opulence. We specialize in crafting spaces that exude elegance, sophistication, and a touch of mystique.",
+      images: {
+        mobile: ourServicesCardOneImageSmall,
+        desktop: ourServicesCardOneImageBig,
+      },
+    },
+    {
+      title: "2: CUSTOM FURNITURE DESIGN",
+      description:
+        "Elevate your space with bespoke furniture tailored to your exact specifications. Whether it's a statement piece or a complete furniture collection, our skilled artisans will bring your unique vision to life.",
+      images: {
+        mobile: ourServicesCardTwoImageSmall,
+        desktop: ourServicesCardTwoImageBig,
+      },
+    },
+    {
+      title: "3: SPACE PLANNING",
+      description:
+        "Our interior design extends beyond aesthetics. We meticulously plan the layout of your space, ensuring it's both aesthetically pleasing and highly functional. Every square foot is optimized for your comfort and practical use.",
+      images: {
+        mobile: ourServicesCardThreeImageSmall,
+        desktop: ourServicesCardThreeImageBig,
+      },
+    },
+    {
+      title: "4: MATERIAL SELECTION",
+      description:
+        "We offer guidance on selecting the finest materials, fabrics, and finishes that align with your design goals, emphasizing durability, aesthetics, and sustainability.",
+      images: {
+        mobile: ourServicesCardFourImageSmall,
+        desktop: ourServicesCardFourImageBig,
+      },
+    },
+    {
+      title: "5: LIGHTING DESIGN",
+      description:
+        "The interplay of light and shadow is essential in dark interiors. We specialize in creating captivating lighting designs that enhance the mood and ambiance of your space.",
+      images: {
+        mobile: ourServicesCardFiveImageSmall,
+        desktop: ourServicesCardFiveImageBig,
+      },
+    },
+    {
+      title: "6: ART CURATION",
+      description:
+        "Elevate your interiors with carefully curated art pieces that complement the overall design. We work with renowned artists and galleries to source artwork that resonates with your style.",
+      images: {
+        mobile: ourServicesCardSixImageSmall,
+        desktop: ourServicesCardSixImageBig,
+      },
+    },
+  ],
+  process: [
+    {
+      title: "1: DISCOVERY AND CONSULTATION",
+      description:
+        "The process begins with a thorough discovery phase where we engage in a one-on-one consultation with our clients. We delve into their aspirations, preferences, and the atmosphere they envision for their space. This stage is crucial in understanding the client's lifestyle, aesthetic preferences, and the unique character of the space.",
+      images: {
+        mobile: ourProcessCardOneImageSmall,
+        desktop: ourProcessCardOneImageBig,
+      },
+    },
+    {
+      title: "2: CONCEPTUALIZATION AND DESIGN DEVELOPMENT",
+      description:
+        "Armed with insights from the consultation, our design team embarks on the conceptualization phase. This involves translating ideas into design concepts that align with the client's vision. Mood boards, color palettes, and preliminary sketches are presented, allowing clients to visualize the proposed design direction. Continuous collaboration ensures that the evolving concepts resonate with the client's expectations.",
+      images: {
+        mobile: ourProcessCardTwoImageSmall,
+        desktop: ourProcessCardTwoImageBig,
+      },
+    },
+    {
+      title: "3: DETAILED PLANNING AND EXECUTION",
+      description:
+        "Once the design concept is approved, we move into the detailed planning phase. This involves meticulous space planning, material selection, and sourcing of bespoke furniture and decor elements. Our team coordinates with skilled craftsmen and artisans to bring the envisioned design to life. Regular client updates and site visits ensure transparency and alignment with the design vision.",
+      images: {
+        mobile: ourProcessCardThreeImageSmall,
+        desktop: ourProcessCardThreeImageBig,
+      },
+    },
+    {
+      title: "4: INSTALLATION AND FINAL TOUCHES",
+      description:
+        "The culmination of our process is the installation phase. Our team orchestrates the placement of furniture, decor, and design elements, ensuring that every detail aligns seamlessly with the envisioned concept. The final touches are added, such as carefully curated accessories and lighting elements that enhance the luxurious ambiance. A final walkthrough with the client ensures their complete satisfaction before unveiling the transformed space.",
+      images: {
+        mobile: ourProcessCardFourImageSmall,
+        desktop: ourProcessCardFourImageBig,
+      },
+    },
+  ],
+};
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,16 +1,17 @@
 import Button from "@/components/Button/Button";
 import styles from "./about.module.scss";
 import { MdArrowOutward } from "react-icons/md";
-import Link from "@/components/Link/Link";
 import heroImg from "@/assets/desktop-about-us-hero.jpg";
+import ourServicesCardOneImageBig from "@/assets/our-service-card-one-big.webp";
+import { aboutUsCardsData } from "@/constants";
 
 function About() {
   return (
-    <>
-      <p className={styles.heading}>
+    <section className={styles.wrapper}>
+      <h1 className={styles.heading}>
         Welcome to Nyeusi Interior Design House, where we redefine the world of
         interior design with our focus on creating dark and luxurious spaces.
-      </p>
+      </h1>
       <div className={styles.img}>
         <img src={heroImg} alt="" />
       </div>
@@ -30,15 +31,52 @@ function About() {
         nuance is not only heard but celebrated. This partnership fuels our
         inspiration and guides our creative direction.
       </p>
-      {/* <Link
-        variant="solid"
-        to="/contact"
-        Icon={MdArrowOutward}
-        className={styles.btn}
-      >
-        Contact us
-      </Link> */}
-    </>
+      <section className={styles.services}>
+        <h2 className={styles.heading}>Our services</h2>
+        <p>
+          Our dedication to quality and aesthetics is reflected in the diverse
+          array of services we offer:
+        </p>
+        <div className={styles.cards}>
+          {aboutUsCardsData.services.map((card, index) => (
+            <div className={styles.card} key={index}>
+              <div>
+                <img
+                  src={card.images.desktop}
+                  alt=""
+                  className={styles["card--img"]}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <h3>{card.title}</h3>
+                <p>{card.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className={`${styles.services} ${styles["mt-50"]}`}>
+        <h2 className={styles.heading}>Our Process</h2>
+        <p>From concept to creation, our concept involves:</p>
+        <div className={styles.cards}>
+          {aboutUsCardsData.process.map((card, index) => (
+            <div className={styles.card} key={index}>
+              <div>
+                <img
+                  src={card.images.desktop}
+                  alt=""
+                  className={styles["card--img"]}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <h3>{card.title}</h3>
+                <p>{card.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </section>
   );
 }
 
