74f1875571ac80bcb61f3bdfbc51b779eb813463
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-05-28 18:05:16 +0100

    feat: add useMediaQuery hook for responsive logic

---
+++ src/hooks/index.js
@@ -0,0 +1,26 @@
+import { useSyncExternalStore } from "react";
+import { useCallback } from "react";
+
+export function useMediaQuery(query) {
+  const subscribe = useCallback(
+    (callback) => {
+      const matchMedia = window.matchMedia(query);
+
+      matchMedia.addEventListener("change", callback);
+      return () => {
+        matchMedia.removeEventListener("change", callback);
+      };
+    },
+    [query]
+  );
+
+  const getSnapshot = () => {
+    return window.matchMedia(query).matches;
+  };
+
+  const getServerSnapshot = () => {
+    throw Error("useMediaQuery is a client-only hook");
+  };
+
+  return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+}
